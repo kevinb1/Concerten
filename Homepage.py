c9f620b313ce1a52a9af659ffebdc0f738615214
Kevin Buinsters <kevinbuinsters@gmail.com>
2025-08-13 14:54:53 +0200

    Deleted all instances of plotly

--- Homepage.py
+++ Homepage.py
@@ -3,7 +3,6 @@ import numpy as np
 import copy
 import streamlit as st
 import altair as alt
-import plotly.express as px
 import datetime as dt
 
 from functions import load_data, edit_data
@@ -26,43 +25,43 @@ if "my_input" not in st.session_state:
     st.session_state["my_input"] = ""
 
 ##################################################################
-####Load Data####
+#### Load Data####
 ##################################################################
 df_concerts = load_data("Concerts.xlsx", sheet_name=0)
 df_collectables = load_data("Collectables.xlsx", sheet_name=0)
 
 ##################################################################
-####Sidebar###
+#### Sidebar###
 ##################################################################
 with st.sidebar:
     st.success("Select a Page Above")
-    
+
     st.header("Filters")
-    
+
     # Filter Band
     band = st.multiselect("Band",
-                            options=df_concerts.Band.unique())
-    
+                          options=df_concerts.Band.unique())
+
     # Filter Venue
     venue = st.multiselect("Venue",
-                            options=df_concerts.Venue.unique())
-    
+                           options=df_concerts.Venue.unique())
+
     # Filter City
     city = st.multiselect("City",
-                            options=df_concerts.City.unique())
-    
+                          options=df_concerts.City.unique())
+
     # Filter Date
-    start_date = st.date_input("Start Date", 
-                               value=None, 
-                               min_value=min(df_concerts.Date), 
+    start_date = st.date_input("Start Date",
+                               value=None,
+                               min_value=min(df_concerts.Date),
                                max_value=max(df_concerts.Date))
-    end_date = st.date_input("End Date", 
-                             value=None, 
-                             min_value=min(df_concerts.Date), 
+    end_date = st.date_input("End Date",
+                             value=None,
+                             min_value=min(df_concerts.Date),
                              max_value=max(df_concerts.Date))
 
 ##################################################################
-###Filter###
+### Filter###
 ##################################################################
 # Apply filters
 query_parts = []
@@ -87,56 +86,58 @@ else:
     df_collectables_selected = df_collectables
 
 ##################################################################
-####Homepage###
+#### Homepage###
 ##################################################################
 st.title("Data Used")
 
 # Show data
 colA1, colA2 = st.columns(2)
 colA1.header("Visited Concerts")
-colA1.data_editor(df_concerts_selected, use_container_width=True, hide_index=True, key="visited_concerts")
+colA1.data_editor(df_concerts_selected, use_container_width=True,
+                  hide_index=True, key="visited_concerts")
 
 colA2.header("Collectables")
-colA2.data_editor(df_collectables_selected, use_container_width=True, hide_index=True, key="collectables")
+colA2.data_editor(df_collectables_selected,
+                  use_container_width=True, hide_index=True, key="collectables")
 
 
 # Create option to edit databases
 st.title("Edit Data")
 colB1, colB2 = st.columns(2)
 selected_table = colB1.radio("Select Table",
-                            ["Visited Concerts", "Collectables"])
+                             ["Visited Concerts", "Collectables"])
 
 edit_type = colB2.radio("Add or Delete Instance",
-                       ["Add", "Delete"])
+                        ["Add", "Delete"])
 
 # Adjust fielst do selection
 if selected_table == "Visited Concerts":
     if edit_type == "Add":
         st.header(f"Add to {selected_table}")
         colC1, colC2, colC3, colC4, colC5, colC6 = st.columns(6)
-        
+
         new_bands = colC1.text_input("Bands Seen")
         new_date = colC2.text_input("Date")
         new_venue = colC3.text_input("Venue")
         new_city = colC4.text_input("City")
         new_price = colC5.text_input("Price")
         new_headliner = colC6.radio("Headliner", ["Yes", "No"])
-        
-        edit_input = {"Band": new_bands, 
+
+        edit_input = {"Band": new_bands,
                       "Date": new_date,
                       "Venue": new_venue,
                       "City": new_city,
                       "Price": new_price,
                       "Headliner": new_headliner}
-        
+
     else:
         st.header(f"Delete from {selected_table}")
         # Eddit_type == "Delete"
         colC1, colC2 = st.columns(2)
-        
+
         delete_bands = colC1.text_input("Bands to Delete")
         delete_date = colC2.text_input("Date")
-        
+
         edit_input = {"Band": delete_bands,
                       "Date": delete_date}
 else:
@@ -144,47 +145,42 @@ else:
     if edit_type == "Add":
         st.header(f"Add to {selected_table}")
         colC1, colC2, colC3, colC4, colC5 = st.columns(5)
-        
+
         new_collectable = colC1.text_input("Collectable")
         new_belongsto = colC2.text_input("Belongs To")
         new_band = colC3.text_input("Band")
         new_date = colC4.text_input("Date")
         new_venue = colC5.text_input("Venue")
-        
-        edit_input = {"Collectable": new_collectable, 
-                      "BelongsTo": new_belongsto, 
-                      "Band": new_band, 
-                      "Date":new_date, 
+
+        edit_input = {"Collectable": new_collectable,
+                      "BelongsTo": new_belongsto,
+                      "Band": new_band,
+                      "Date": new_date,
                       "Venue": new_venue}
-        
+
     else:
         # Eddit_type == "Delete"
         st.header(f"Delete from {selected_table}")
-        
+
         colC1, colC2, colC3 = st.columns(3)
-        
-        delete_collectable= colC1.text_input("Collectable to Delete")
+
+        delete_collectable = colC1.text_input("Collectable to Delete")
         delete_belongsto = colC2.text_input("Belongs To")
         delete_date = colC3.text_input("Date")
-        
+
         edit_input = {"Collectable": delete_collectable,
                       "BelongsTo": delete_belongsto,
                       "Date": delete_date}
 
 if st.button(f"Aply Changes", type="primary"):
     if selected_table == "Visited Concerts":
-        df_concerts_edited = edit_data(df_concerts_selected, edit_type, edit_input)
+        df_concerts_edited = edit_data(
+            df_concerts_selected, edit_type, edit_input)
         df_concerts_edited.to_excel("Concerts.xlsx", index=False)
     else:
-        df_collectables_edited = edit_data(df_collectables_selected, edit_type, edit_input)
+        df_collectables_edited = edit_data(
+            df_collectables_selected, edit_type, edit_input)
         df_collectables_edited.to_excel("Collectables.xlsx", index=False)
-        
-        
 
     # st.experimental_rerun()
     st.rerun()
-
-
-
-
-
